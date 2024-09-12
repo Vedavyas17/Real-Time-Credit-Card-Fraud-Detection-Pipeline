bc02d787da0323f76cb1ce6caf53d430db3aa966
Vedavyas17 <vedavyascrp@gmail.com>
2024-09-12 12:20:43 -0400

    driver update

--- src/driver.py
+++ src/driver.py
@@ -4,3 +4,20 @@ from db.dao import *
 if __name__ == "__main__":
     # Read data from Kafka Topic
     consumer = readKafka()
+    
+    # Loop through Kafka messages
+    for message in consumer:
+        # Get actual message
+        currentMessage = message.value
+        
+        # Determine if the transaction is fraudulent or not
+        status = processMessage(currentMessage)
+        
+        # Add status to the message
+        currentMessage["status"] = status
+        
+        # Print the current message status
+        print(currentMessage)
+        
+        # Write the message to MongoDB
+        writeToMongo(currentMessage)
