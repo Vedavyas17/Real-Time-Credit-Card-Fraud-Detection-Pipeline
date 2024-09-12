c5229f523714c7b3fcd9f1f0beeef32c4ed62ece
Vedavyas17 <vedavyascrp@gmail.com>
2024-09-12 12:22:19 -0400

    mongoDB connection creation

--- src/db/dao.py
+++ src/db/dao.py
@@ -4,3 +4,16 @@ from json import loads
 
 client = MongoClient('localhost', 27017)
 db = client['capstoneproject']
+
+def readMongoLookup():
+    try:
+        lookup_transaction = db.lookupTrans
+        print("Connection Successful")
+    except:
+        print("Connection to MongoDB is unsuccessful")
+    return lookup_transaction
+
+def writeToMongo(transaction):
+    collection_name = db.cardTransactions
+    rec_id = collection_name.insert_one(transaction)
+    print("The data is inserted with Record ID", rec_id)
